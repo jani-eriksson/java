0dd8a02fe0ce1a46dca50e5123bc6663fd4a5aef
jani-eriksson <72618353+jani-eriksson@users.noreply.github.com>
2021-01-24 18:33:45 +0200

    Hello world

---
+++ QuickStart.java
@@ -0,0 +1,9 @@
+class QuickStart {
+    public static void main(String[] args) {
+        System.out.println("To whom it may concern");
+        System.out.println("    -   ");
+        System.out.println("Writing in Java");
+        System.out.println("    -   ");
+        System.out.println("By Jani");
+    }
+}
