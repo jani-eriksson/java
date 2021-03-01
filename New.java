9afbc8fd5a39f46a14fbf740c182c0bc6cf87dcc
jani-eriksson <72618353+jani-eriksson@users.noreply.github.com>
2021-03-01 20:04:21 +0200

    Print loop with for

---
+++ New.java
@@ -0,0 +1,8 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println("Print 1 multiplied by 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
