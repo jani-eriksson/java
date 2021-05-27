204a474da32063815a8d939ffe653da208600e7e
jani-eriksson <72618353+jani-eriksson@users.noreply.github.com>
2021-05-27 18:02:26 +0300

    Simplifying

--- UserInterface.java
+++ UserInterface.java
@@ -9,13 +9,33 @@ public class UserInterface {
 
     public void start() {
         while (true) {
-            System.out.println("testing phase, write x to escape");
-            String command = scan.nextLine();
-            
-            if (command.equals("x")) {
+            startMenu();
+            String command = scan.nextLine().toUpperCase();
+            commands(command);
+            if (command.equals("END")) {
                 break;
             }
         }
+    }
+
+    public void startMenu() {
+        System.out.println("  -  -  -  - Commands -  -  -  -");
+        System.out.println(" | Start  -->  in order to begin |");
+        System.out.println(" |  End   -->  close the program |");
+        System.out.println("  -  -  -  -  -  -  -  -  -  -  -");
+    }
+
+    public void commands(String command) {
+        if (!(command.equals("START") || command.equals("END"))) {
+            System.out.println("Incorrect input. Please, try again. \n");
+        }
+        if (command.equals("START")) {
+            System.out.println("Starting...");
+        }
+        if (command.equals("END")) {
+            System.out.println("Closing...");
+        }
+        
     }
 
 }
